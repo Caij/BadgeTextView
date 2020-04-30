d13820cd07cbd561733d69295bd669aff22f8c53
cj250962 <cj250962@alibaba-inc.com>
2020-04-30 13:20:12 +0800

    优化显示问题

--- lib/src/main/java/com/caij/badge/BadgeTextView.java
+++ lib/src/main/java/com/caij/badge/BadgeTextView.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.graphics.drawable.shapes.RectShape;
 import android.graphics.drawable.shapes.RoundRectShape;
 import android.util.AttributeSet;
+import android.util.LruCache;
 import android.view.Gravity;
 import android.widget.TextView;
+
+import java.util.Map;
 
 
 /**
@@ -20,7 +24,13 @@ import android.widget.TextView;
 public class BadgeTextView extends TextView {
 
     private int badgeColor;
-    private int preTextSize = 0;
+
+    private static LruCache<String, ShapeDrawable> sDrawableLruCache = new LruCache<String, ShapeDrawable>(20){
+        @Override
+        protected int sizeOf(String key, ShapeDrawable value) {
+            return 1;
+        }
+    };
 
     public BadgeTextView(Context context) {
         this(context, null);
@@ -48,19 +58,27 @@ public class BadgeTextView extends TextView {
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int height = getMeasuredHeight();
+        int width = getMeasuredWidth();
+        if (width < height) {
+            int w = Math.max(height, width);
+            super.onMeasure(MeasureSpec.makeMeasureSpec(w, MeasureSpec.EXACTLY),
+                    MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY));
+        }
+    }
+
+    @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        int currentTextSize = getCSize(getText());
-        if (currentTextSize != this.preTextSize) {
-            refreshBackgroundDrawable();
-            this.preTextSize = currentTextSize;
-        }
+        refreshBackgroundDrawable(w, h);
     }
 
     public void setBadgeColor(int colorInt) {
         if (badgeColor != colorInt) {
             this.badgeColor = colorInt;
-            refreshBackgroundDrawable();
+            refreshBackgroundDrawable(getWidth(), getHeight());
         }
     }
 
@@ -69,24 +87,29 @@ public class BadgeTextView extends TextView {
         return charSequence.length();
     }
 
-    private void refreshBackgroundDrawable() {
+    private void refreshBackgroundDrawable(int w, int h) {
         CharSequence text = getText();
         if (text == null) {
             return;
         }
 
-        RectShape rectShape;
-        if (text.length() == 1) {
-            rectShape = new Oval();
-        }else {
-            // 外部矩形弧度
-            int ra = getHeight() / 2;
-            float[] outerR = new float[] {ra, ra, ra, ra, ra, ra, ra, ra };
-            rectShape = new RoundRectShape(outerR, null, null);
+        String key = w + "*" + h + badgeColor;
+
+        ShapeDrawable drawable = sDrawableLruCache.get(key);
+        if (drawable == null) {
+            RectShape rectShape;
+            if (text.length() == 1) {
+                rectShape = new Oval();
+            }else {
+                // 外部矩形弧度
+                int ra = getHeight() / 2;
+                float[] outerR = new float[] {ra, ra, ra, ra, ra, ra, ra, ra };
+                rectShape = new RoundRectShape(outerR, null, null);
+            }
+            drawable = new ShapeDrawable(rectShape);
+            drawable.getPaint().setColor(badgeColor);
         }
-        ShapeDrawable shapeDrawable = new ShapeDrawable(rectShape);
-        shapeDrawable.getPaint().setColor(badgeColor);
-        setBackgroundDrawable(shapeDrawable);
+        setBackgroundDrawable(drawable);
     }
 
     private class Oval extends OvalShape {
